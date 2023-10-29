7ea16f6f7f5888d9b9ac60c1cca0824467b1a02b
adelina <adelina.pasculescu@student.upt.ro>
2023-10-29 16:53:19 +0200

    initializing  cmake project

---
+++ build/CMakeFiles/adelina_paoo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/adelina_paoo.dir/main.cpp.o"
+  "CMakeFiles/adelina_paoo.dir/main.cpp.o.d"
+  "adelina_paoo"
+  "adelina_paoo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/adelina_paoo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/adelina_paoo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for adelina_paoo.
+# This may be replaced when dependencies are built.
